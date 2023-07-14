bc6d97cc142310d37f23a14993c12cda2d6562fa
Kim Boram <bory2321@naver.com>
2023-07-15 08:02:46 +0900

    비밀번호 등급 설정

---
+++ components/pwdgrade.js
@@ -0,0 +1,15 @@
+const Em = styled.em`
+  width: 40px;
+  font-size: 12px;
+  background: rgba(3, 199, 90, 0.12);
+  color: #03c75a;
+  font-style: normal;
+  border-radius: 10px;
+  text-align: center;
+  font-weight: bold;
+  padding: 4px 6px 3px 6px;
+  line-height: 13px;
+  letter-spacing: -0.5px;
+`;
+
+// 패스워드를 분석해서 등급을 나누고 나눈 등급으로 텍스트와 색깔을 바꿔서 보여주도록
